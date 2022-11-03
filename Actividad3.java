838a20a6c734f73a0ca28664b2120a9a6f54908d
alvarodiaz12 <a_alvaro.diaz.casano@iespablopicasso.es>
2022-11-03 18:58:53 +0100

    Archivo java Actividad3 modificado

--- Actividad3.java
+++ Actividad3.java
@@ -6,55 +6,16 @@ public class Actividad3{
     
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("Indique que valores quieres que se usen para realizar la siguiente"
-        + "operación, siendo la letra S para la suma, R para la"
-        + "resta, P para el producto y D para la división. ");
+    System.out.println("Dime el valor de valor1:");
+    int valor1 = Integer.parseInt(sc.next());
     
-    char operacion = sc.next().charAt(0);
-    double valor1;
-    double valor2; 
-    switch (operacion) {
-      case 'S':
-        System.out.println("Indique el primer valor");
-        valor1 = Double.parseDouble(sc.next());
-        
-        System.out.println("Indique el primer valor");
-        valor2 = Double.parseDouble(sc.next());
-        
-        double suma = valor1 + valor2;
-        System.out.println("El resultado es " + suma);
-      break;
-      case 'R':
-        System.out.println("Indique el primer valor");
-        valor1 = Double.parseDouble(sc.next());
-      
-        System.out.println("Indique el primer valor");
-        valor2 = Double.parseDouble(sc.next());
-        
-        double resta = valor1 - valor2;
-        System.out.println("El resultado es " + resta);
-      break;
-      case 'P':
-        System.out.println("Indique el primer valor");
-        valor1 = Double.parseDouble(sc.next());
-      
-        System.out.println("Indique el primer valor");
-        valor2 = Double.parseDouble(sc.next());
-      
-        double producto = valor1 * valor2;
-        System.out.println("El resultado es " + producto);
-      break;
-      case 'D':
-      System.out.println("Indique el primer valor");
-      valor1 = Double.parseDouble(sc.next());
-      
-      System.out.println("Indique el primer valor");
-      valor2 = Double.parseDouble(sc.next());
-      
-      double division = valor1 / valor2;
-      System.out.println("El resultado es " + division);
-      break;
-      }
+    System.out.println("Dime el valor de valor2:");
+    int valor2 = Integer.parseInt(sc.next());
+    
+    int suma = valor1 + valor2;
+    
+    System.out.println("La suma de " + valor1 + " y " + valor2 + " es " + suma);
+    
     sc.close();
     
   }
